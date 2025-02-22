a9b9a640880e510d9c469b8ea9fdd6f2ae2d9fb6
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-02-22 17:52:02 +0100

    Enhance tooltip functionality with improved status bar positioning and backdrop setup

--- Modules/Tooltips.lua
+++ Modules/Tooltips.lua
@@ -1,14 +1,30 @@
-local function UpdateTooltip(tooltip)
-    if tooltip:GetAnchorType() ~= "ANCHOR_CURSOR" then
-        tooltip:ClearAllPoints()
-        tooltip:SetPoint("TOPLEFT", TargetFramePortrait, "BOTTOMRIGHT", 4, -4)
+local function UpdateTooltip(self)
+    if self:GetAnchorType() ~= "ANCHOR_CURSOR" then
+        GameTooltip:ClearAllPoints()
+        GameTooltip:SetPoint("TOPLEFT", TargetFramePortrait, "BOTTOMRIGHT", 4, -4)
+        
         GameTooltipStatusBar:ClearAllPoints()
-        GameTooltipStatusBar:SetPoint("TOP", tooltip, "BOTTOM", 0, 4)
+        GameTooltipStatusBar:SetPoint("TOPLEFT", GameTooltip, "BOTTOMLEFT", 4, -4)
+        GameTooltipStatusBar:SetPoint("TOPRIGHT", GameTooltip, "BOTTOMRIGHT", -4, -4)
+        GameTooltipStatusBar:SetHeight(12)
         GameTooltipStatusBar:SetStatusBarTexture("Interface/RaidFrame/Raid-Bar-HP-Fill.blp")
-        GameTooltipStatusBar:SetFrameLevel(tooltip:GetFrameLevel() - 1)
+        GameTooltipStatusBar:SetFrameLevel(GameTooltip:GetFrameLevel() + 1)
+        
+        local TooltipStatusBarBackdrop = GameTooltipStatusBar.Backdrop
+        if not TooltipStatusBarBackdrop then
+            TooltipStatusBarBackdrop = CreateFrame("Frame", nil, GameTooltipStatusBar, "BackdropTemplate")
+            TooltipStatusBarBackdrop:SetBackdrop({
+                edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
+                edgeSize = 12
+            })
+            TooltipStatusBarBackdrop:SetBackdropBorderColor(0.5, 0.5, 0.5, 1)
+            TooltipStatusBarBackdrop:SetFrameLevel(GameTooltipStatusBar:GetFrameLevel() + 1)
+            GameTooltipStatusBar.Backdrop = TooltipStatusBarBackdrop
+        end
+        TooltipStatusBarBackdrop:ClearAllPoints()
+        TooltipStatusBarBackdrop:SetPoint("TOPLEFT", GameTooltipStatusBar, "TOPLEFT", -3, 3)
+        TooltipStatusBarBackdrop:SetPoint("BOTTOMRIGHT", GameTooltipStatusBar, "BOTTOMRIGHT", 3, -3)
     end
 end
 
 hooksecurefunc("GameTooltip_SetDefaultAnchor", UpdateTooltip)
-GameTooltip:HookScript("OnTooltipSetUnit", UpdateTooltip)
-GameTooltip:HookScript("OnSizeChanged", function() GameTooltipStatusBar:Hide() end)
