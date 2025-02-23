c496da83324289c62a84f37d8f0c4aa93a46e7d0
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-02-23 10:45:53 +0100

    Refactor PlayerFrame module for improved update functions and event handling

--- Modules/PlayerFrame.lua
+++ Modules/PlayerFrame.lua
@@ -23,7 +23,7 @@ PlayerPortraitBackdrop:SetAttribute("type1", "target")
 PlayerPortraitBackdrop:SetAttribute("type2", "togglemenu")
 
 -- Player Frame Update Function
-local function PlayerFrameUpdate()
+local function UpdatePlayerFrame()
 	-- Position and sizing updates
     PlayerFrame:ClearAllPoints()
     PlayerFrame:SetPoint("TOPLEFT", PlayerPortraitBackdrop, "TOPLEFT", 0, 0)
@@ -48,22 +48,32 @@ local function PlayerFrameUpdate()
     PlayerPVPTimerText:Hide()
     PlayerPVPIcon:SetAlpha(0)
 
-    -- Font and Text adjustments for player name, health and mana bars
     PlayerName:ClearAllPoints()
     PlayerName:SetPoint("TOP", PlayerFrameBackdrop, "TOP", 0, -5)
     PlayerName:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
     PlayerName:SetTextColor(1, 1, 1, 1)
+end
 
+-- Event Registrations for Player Frame
+local PlayerFrameEvents = CreateFrame("Frame")
+PlayerFrameEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
+PlayerFrameEvents:SetScript("OnEvent", UpdatePlayerFrame)
+
+-- Player Resource Update Function
+local function UpdatePlayerResource()
+    -- Update player health bar
     PlayerFrameHealthBar:ClearAllPoints()
     PlayerFrameHealthBar:SetSize(PlayerFrameBackground:GetWidth(), 16)
     PlayerFrameHealthBar:SetPoint("BOTTOM", PlayerFrameManaBar, "TOP", 0, 0)
     PlayerFrameHealthBar:SetStatusBarTexture("Interface/RaidFrame/Raid-Bar-HP-Fill.blp")
-    
+
+    -- Update player mana bar
     PlayerFrameManaBar:ClearAllPoints()
     PlayerFrameManaBar:SetPoint("BOTTOM", PlayerFrameBackdrop, "BOTTOM", 0, 4)
     PlayerFrameManaBar:SetSize(PlayerFrameBackground:GetWidth(), 8)
     PlayerFrameManaBar:SetStatusBarTexture("Interface/RaidFrame/Raid-Bar-HP-Fill.blp")
 
+    -- Update health bar text
     PlayerFrameHealthBarText:SetPoint("CENTER", PlayerFrameHealthBar, "CENTER", 0, 0)
     PlayerFrameHealthBarText:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
     PlayerFrameHealthBarTextRight:SetPoint("RIGHT", PlayerFrameHealthBar, "RIGHT", -4, 0)
@@ -71,23 +81,27 @@ local function PlayerFrameUpdate()
     PlayerFrameHealthBarTextLeft:SetPoint("LEFT", PlayerFrameHealthBar, "LEFT", 4, 0)
     PlayerFrameHealthBarTextLeft:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
 
+    -- Update mana bar text
     PlayerFrameManaBarText:SetPoint("CENTER", PlayerFrameManaBar, "CENTER", 0, 0)
     PlayerFrameManaBarText:SetFont(STANDARD_TEXT_FONT, 8, "OUTLINE")
     PlayerFrameManaBarTextLeft:SetPoint("LEFT", PlayerFrameManaBar, "LEFT", 4, 0)
     PlayerFrameManaBarTextLeft:SetFont(STANDARD_TEXT_FONT, 8, "OUTLINE")
     PlayerFrameManaBarTextRight:SetPoint("RIGHT", PlayerFrameManaBar, "RIGHT", -4, 0)
     PlayerFrameManaBarTextRight:SetFont(STANDARD_TEXT_FONT, 8, "OUTLINE")
-
 end
 
--- Event Registrations for Player Frame
-local PlayerFrameEvents = CreateFrame("Frame")
-PlayerFrameEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
-PlayerFrameEvents:RegisterEvent("PLAYER_REGEN_DISABLED")
-PlayerFrameEvents:RegisterEvent("PLAYER_REGEN_ENABLED")
-PlayerFrameEvents:SetScript("OnEvent", PlayerFrameUpdate)
+-- Player Resource Events
+local PlayerResourceEvents = CreateFrame("Frame")
+PlayerResourceEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
+PlayerResourceEvents:RegisterEvent("UNIT_MANA")
+PlayerResourceEvents:RegisterEvent("UNIT_HEALTH")
+PlayerResourceEvents:SetScript("OnEvent", function(self, event, arg1)
+    if event == "PLAYER_ENTERING_WORLD" or (arg1 == "player" and (event == "UNIT_MANA" or event == "UNIT_HEALTH")) then
+        UpdatePlayerResource()
+    end
+end)
 
--- Player Portrait Update Function and Events
+-- Player Portrait Update Function
 local function PlayerPortraitUpdate()
     PlayerPortrait:ClearAllPoints()
     PlayerPortrait:SetPoint("CENTER", PlayerPortraitBackdrop, "CENTER", 0, 0)
@@ -109,12 +123,13 @@ local function PlayerPortraitUpdate()
     PlayerPVPTimerText:SetTextColor(1, 1, 1, 1)
 end
 
+-- Player Portrait Events
 local PlayerPortraitEvents = CreateFrame("Frame")
 PlayerPortraitEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
 PlayerPortraitEvents:SetScript("OnEvent", PlayerPortraitUpdate)
 
--- Player Group Update Function and Events
-local function PlayerGroupUpdate()
+-- Player Group Update Function
+local function UpdateGroupElements()
     PlayerFrameGroupIndicator:SetAlpha(0)
     PlayerFrameGroupIndicator:Hide()
 
@@ -126,17 +141,19 @@ local function PlayerGroupUpdate()
     end
 end
 
+-- Hook Group Elements Update
 if _G["MultiGroupFrame"] then
-    MultiGroupFrame:HookScript("OnShow", PlayerGroupUpdate)
+    MultiGroupFrame:HookScript("OnShow", UpdateGroupElements)
 end
 
-PlayerFrameGroupIndicator:HookScript("OnShow", PlayerGroupUpdate)
+PlayerFrameGroupIndicator:HookScript("OnShow", UpdateGroupElements)
 
+-- Player Group Events
 local PlayerGroupEvents = CreateFrame("Frame")
 PlayerGroupEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
-PlayerGroupEvents:SetScript("OnEvent", PlayerGroupUpdate)
+PlayerGroupEvents:SetScript("OnEvent", UpdateGroupElements)
 
--- Player Level Update Function and Events
+-- Player Level Update Function
 local function PlayerLevelUpdate()
     PlayerLevelText:ClearAllPoints()
     PlayerLevelText:SetPoint("TOP", PlayerPortraitBackdrop, "BOTTOM", 0, -4)
@@ -150,6 +167,7 @@ local function PlayerLevelUpdate()
     end
 end
 
+-- Player Level Events
 local PlayerLevelEvents = CreateFrame("Frame")
 PlayerLevelEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
 PlayerLevelEvents:RegisterEvent("PLAYER_LEVEL_UP")
