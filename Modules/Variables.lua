7022f5f6d0352d55fc1176c0a0b7c65612b4cb39
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-02-25 17:28:41 +0100

    Refactor color definitions in Variables module: consolidate comments and improve readability

--- Modules/Variables.lua
+++ Modules/Variables.lua
@@ -1,43 +1,27 @@
--- DEFINE GLOBAL COLORS
+-- DEFINE COLORS
 
 RGB = {
-    -- #2871D9
-    BLUE = "0.157, 0.443, 0.851",
-    -- #FBD134
-    YELLOW = "0.984, 0.820, 0.204",
-    -- #FDFE9E
-    BEIGE = "0.992, 0.996, 0.620",
-    -- #24FE31
-    GREEN = "0.141, 0.996, 0.192",
-    -- #A43BEA
-    PURPLE = "0.643, 0.231, 0.918",
-    -- #FB18B2
-    PINK = "0.984, 0.094, 0.698",
-    -- #FFFFFF
-    WHITE = "1.000, 1.000, 1.000",
-    -- #808080
-    GREY = "0.5, 0.5, 0.5",
-    -- #EA2029
-    RED = "0.918, 0.125, 0.161",
+    BLUE = "0.157, 0.443, 0.851",   -- #2871D9
+    YELLOW = "0.984, 0.820, 0.204", -- #FBD134
+    BEIGE = "0.992, 0.996, 0.620",  -- #FDFE9E
+    RED = "0.918, 0.125, 0.161",    -- #EA2029
+    PURPLE = "0.643, 0.231, 0.918", -- #A43BEA
+    PINK = "0.984, 0.094, 0.698",   -- #FB18B2
+    GREEN = "0.141, 0.996, 0.192",  -- #24FE31
+    BORDER = "0.5, 0.5, 0.5, 1",    -- #808080
+    GREY = "0.5, 0.5, 0.5",         -- #808080
+    WHITE = "1, 1, 1",              -- #FFFFFF
 }
-    
+
 LUA = {
-    -- #2871D9
-    BLUE = "|cFF2871D9",
-    -- #FBD134
-    YELLOW = "|cFFFBD134",
-    -- #FDFE9E
-    BEIGE = "|cFFFDFE9E",
-    -- #24FE31
-    GREEN = "|cFF24FE31",
-    -- #A43BEA
-    PURPLE = "|cFFA43BEA",
-    -- #FB18B2
-    PINK = "|cFFFB18B2",
-    -- #FFFFFF
-    WHITE = "|cFFFFFFFF",
-    -- #808080
-    GREY = "|cFF808080",
-    -- #EA2029
-    RED = "|cFFEA2029",
+    BLUE = "|cFF2871D9",            -- #2871D9
+    YELLOW = "|cFFFBD134",          -- #FBD134
+    BEIGE = "|cFFFDFE9E",           -- #FDFE9E
+    RED = "|cFFEA2029",             -- #EA2029
+    PURPLE = "|cFFA43BEA",          -- #A43BEA
+    PINK = "|cFFFB18B2",            -- #FB18B2
+    GREEN = "|cFF24FE31",           -- #24FE31
+    BORDER = "|cFF808080",          -- #808080
+    GREY = "|cFF808080",            -- #808080
+    WHITE = "|cFFFFFFFF",           -- #FFFFFF
 }
