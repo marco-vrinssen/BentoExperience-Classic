3d8bbc2d3c37bac10241e1003543e0e7fac96f95
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-02-27 11:25:15 +0100

    Refactor CastBar module: update backdrop creation, improve positioning, and enhance color handling

--- Modules/CastBar.lua
+++ Modules/CastBar.lua
@@ -1,53 +1,40 @@
--- GENERATE CASTBAR BACKDROP
+-- CREATE CASTBAR BACKDROP
 
 local CastingBarBackdrop = CreateFrame("Frame", nil, CastingBarFrame, "BackdropTemplate")
 CastingBarBackdrop:SetPoint("TOPLEFT", CastingBarFrame, "TOPLEFT", -3, 3)
 CastingBarBackdrop:SetPoint("BOTTOMRIGHT", CastingBarFrame, "BOTTOMRIGHT", 3, -3)
-CastingBarBackdrop:SetBackdrop({ edgeFile = EDGE, edgeSize = 12 })
-CastingBarBackdrop:SetBackdropBorderColor(unpack(RGB.GREY))
+CastingBarBackdrop:SetBackdrop({ edgeFile = T.EDGE, edgeSize = T.EDGE_SIZE})
+CastingBarBackdrop:SetBackdropBorderColor(unpack(N.RGB))
 CastingBarBackdrop:SetFrameStrata("HIGH")
 
 
-
-
--- UPDATE CAST BAR
+-- UPDATE CASTBAR
 
 local function UpdateCastBar()
     CastingBarFrame:ClearAllPoints()
     CastingBarFrame:SetSize(160, 24)
     CastingBarFrame:SetMovable(true)
     CastingBarFrame:SetUserPlaced(true)
-    CastingBarFrame:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, 228)
-    CastingBarFrame:SetStatusBarTexture("Interface/RaidFrame/Raid-Bar-HP-Fill.blp")
+    CastingBarFrame:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, 240)
+    CastingBarFrame:SetStatusBarTexture(T.BAR)
     CastingBarFrame.Border:Hide()
     CastingBarFrame.Spark:SetTexture(nil)
     CastingBarFrame.Flash:SetTexture(nil)
     CastingBarFrame.Text:ClearAllPoints()
     CastingBarFrame.Text:SetPoint("CENTER", CastingBarFrame, "CENTER", 0, 0)
-    CastingBarFrame.Text:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
-end  
-
-
-
-
--- RECOLOR CAST BAR ON START
+    CastingBarFrame.Text:SetFont(F.TYPE, F.SIZE, "OUTLINE")
+end
 
 local function RecolorCastBar(event)
     if event == "UNIT_SPELLCAST_START" then
-        CastingBarFrame:SetStatusBarColor(unpack(RGB.YELLOW))
+        CastingBarFrame:SetStatusBarColor(unpack(Y.RGB))
     end
 end  
 
-
-
-
--- DEFINE CASTBAR EVENTS
-
-local CastBarEvents = CreateFrame("Frame")
-CastBarEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
-CastBarEvents:RegisterEvent("UNIT_SPELLCAST_START")
-CastBarEvents:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
-CastBarEvents:SetScript("OnEvent", function(self, event, ...)
+local CastBarFrame = CreateFrame("Frame")
+CastBarFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+CastBarFrame:RegisterEvent("UNIT_SPELLCAST_START")
+CastBarFrame:SetScript("OnEvent", function(self, event, ...)
     if event == "PLAYER_ENTERING_WORLD" then
         UpdateCastBar()
     else
