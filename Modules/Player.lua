7d1454478b464a076a2be3e01117cf3b8f964f9c
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-02-27 11:26:12 +0100

    Split up components

--- Modules/Player.lua
+++ Modules/Player.lua
@@ -1,29 +1,26 @@
--- GENERATE PLAYER BACKDROPS
+-- CREATE PLAYER FRAME BACKDROPS
 
-local PlayerFrameBackdrop = CreateFrame("Button", nil, PlayerFrame, "SecureUnitButtonTemplate, BackdropTemplate")
-PlayerFrameBackdrop:SetPoint("BOTTOM", UIParent, "BOTTOM", -190, 224)
+PlayerFrameBackdrop = CreateFrame("Button", nil, PlayerFrame, "SecureUnitButtonTemplate, BackdropTemplate")
+PlayerFrameBackdrop:SetPoint("BOTTOM", UIParent, "BOTTOM", -190, 240)
 PlayerFrameBackdrop:SetSize(124, 48)
-PlayerFrameBackdrop:SetBackdrop({edgeFile = "Interface/Tooltips/UI-Tooltip-Border", edgeSize = 12})
-PlayerFrameBackdrop:SetBackdropBorderColor(0.5, 0.5, 0.5, 1)
+PlayerFrameBackdrop:SetBackdrop({edgeFile = T.EDGE, edgeSize = T.EDGE_SIZE})
+PlayerFrameBackdrop:SetBackdropBorderColor(unpack(N.RGB))
 PlayerFrameBackdrop:SetFrameStrata("HIGH")
 PlayerFrameBackdrop:SetAttribute("unit", "player")
 PlayerFrameBackdrop:RegisterForClicks("AnyUp")
 PlayerFrameBackdrop:SetAttribute("type1", "target")
 PlayerFrameBackdrop:SetAttribute("type2", "togglemenu")
 
-local PlayerPortraitBackdrop = CreateFrame("Button", nil, PlayerFrame, "SecureUnitButtonTemplate, BackdropTemplate")
+PlayerPortraitBackdrop = CreateFrame("Button", nil, PlayerFrame, "SecureUnitButtonTemplate, BackdropTemplate")
 PlayerPortraitBackdrop:SetPoint("RIGHT", PlayerFrameBackdrop, "LEFT", 0, 0)
 PlayerPortraitBackdrop:SetSize(48, 48)
-PlayerPortraitBackdrop:SetBackdrop({edgeFile = "Interface/Tooltips/UI-Tooltip-Border", edgeSize = 12})
-PlayerPortraitBackdrop:SetBackdropBorderColor(0.5, 0.5, 0.5, 1)
+PlayerPortraitBackdrop:SetBackdrop({edgeFile = T.EDGE, edgeSize = T.EDGE_SIZE})
+PlayerPortraitBackdrop:SetBackdropBorderColor(unpack(N.RGB))
 PlayerPortraitBackdrop:SetFrameStrata("HIGH")
 PlayerPortraitBackdrop:SetAttribute("unit", "player")
 PlayerPortraitBackdrop:RegisterForClicks("AnyUp")
 PlayerPortraitBackdrop:SetAttribute("type1", "target")
 PlayerPortraitBackdrop:SetAttribute("type2", "togglemenu")
-
-
-
 
 -- UPDATE PLAYER FRAME
 
@@ -53,65 +50,95 @@ local function UpdatePlayerFrame()
 
     PlayerName:ClearAllPoints()
     PlayerName:SetPoint("TOP", PlayerFrameBackdrop, "TOP", 0, -5)
-    PlayerName:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
-    PlayerName:SetTextColor(1, 1, 1, 1)
+    PlayerName:SetFont(F.TYPE, F.SIZE, "OUTLINE")
+    PlayerName:SetTextColor(unpack(W.RGB))
+
+    PlayerLevelText:ClearAllPoints()
+    PlayerLevelText:SetPoint("TOP", PlayerPortraitBackdrop, "BOTTOM", 0, -4)
+    PlayerLevelText:SetFont(F.TYPE, F.SIZE, "OUTLINE")
+    PlayerLevelText:SetTextColor(unpack(W.RGB))
+
+    if UnitLevel("player") == MAX_PLAYER_LEVEL then
+        PlayerLevelText:Hide()
+    else
+        PlayerLevelText:Show()
+    end
+
+    PlayerFrameHealthBarText:SetPoint("CENTER", PlayerFrameHealthBar, "CENTER", 0, 0)
+    PlayerFrameHealthBarText:SetFont(F.TYPE, F.SIZE, "OUTLINE")
+    PlayerFrameHealthBarTextRight:SetPoint("RIGHT", PlayerFrameHealthBar, "RIGHT", -4, 0)
+    PlayerFrameHealthBarTextRight:SetFont(F.TYPE, F.SIZE, "OUTLINE")
+    PlayerFrameHealthBarTextLeft:SetPoint("LEFT", PlayerFrameHealthBar, "LEFT", 4, 0)
+    PlayerFrameHealthBarTextLeft:SetFont(F.TYPE, F.SIZE, "OUTLINE")
+
+    PlayerFrameManaBarText:SetPoint("CENTER", PlayerFrameManaBar, "CENTER", 0, 0)
+    PlayerFrameManaBarText:SetFont(F.TYPE, 8, "OUTLINE")
+    PlayerFrameManaBarTextLeft:SetPoint("LEFT", PlayerFrameManaBar, "LEFT", 4, 0)
+    PlayerFrameManaBarTextLeft:SetFont(F.TYPE, 8, "OUTLINE")
+    PlayerFrameManaBarTextRight:SetPoint("RIGHT", PlayerFrameManaBar, "RIGHT", -4, 0)
+    PlayerFrameManaBarTextRight:SetFont(F.TYPE, 8, "OUTLINE")
 end
 
-local PlayerFrameEvents = CreateFrame("Frame")
-PlayerFrameEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
-PlayerFrameEvents:SetScript("OnEvent", UpdatePlayerFrame)
+local PlayerFrameFrame = CreateFrame("Frame")
+PlayerFrameFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+PlayerFrameFrame:SetScript("OnEvent", UpdatePlayerFrame)
 
 
 
 
--- UPDATE PLAYER RESOURCE BARS
 
-local function UpdatePlayerResource()
+
+
+
+local function UpdatePlayerResources()
+
     PlayerFrameHealthBar:ClearAllPoints()
     PlayerFrameHealthBar:SetSize(PlayerFrameBackground:GetWidth(), 16)
     PlayerFrameHealthBar:SetPoint("BOTTOM", PlayerFrameManaBar, "TOP", 0, 0)
-    PlayerFrameHealthBar:SetStatusBarTexture("Interface/RaidFrame/Raid-Bar-HP-Fill.blp")
+    PlayerFrameHealthBar:SetStatusBarTexture(T.BAR)
+    PlayerFrameHealthBar:SetStatusBarColor(unpack(G.RGB))
 
     PlayerFrameManaBar:ClearAllPoints()
     PlayerFrameManaBar:SetPoint("BOTTOM", PlayerFrameBackdrop, "BOTTOM", 0, 4)
     PlayerFrameManaBar:SetSize(PlayerFrameBackground:GetWidth(), 8)
-    PlayerFrameManaBar:SetStatusBarTexture("Interface/RaidFrame/Raid-Bar-HP-Fill.blp")
-
-    PlayerFrameHealthBarText:SetPoint("CENTER", PlayerFrameHealthBar, "CENTER", 0, 0)
-    PlayerFrameHealthBarText:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
-    PlayerFrameHealthBarTextRight:SetPoint("RIGHT", PlayerFrameHealthBar, "RIGHT", -4, 0)
-    PlayerFrameHealthBarTextRight:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
-    PlayerFrameHealthBarTextLeft:SetPoint("LEFT", PlayerFrameHealthBar, "LEFT", 4, 0)
-    PlayerFrameHealthBarTextLeft:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
-
-    PlayerFrameManaBarText:SetPoint("CENTER", PlayerFrameManaBar, "CENTER", 0, 0)
-    PlayerFrameManaBarText:SetFont(STANDARD_TEXT_FONT, 8, "OUTLINE")
-    PlayerFrameManaBarTextLeft:SetPoint("LEFT", PlayerFrameManaBar, "LEFT", 4, 0)
-    PlayerFrameManaBarTextLeft:SetFont(STANDARD_TEXT_FONT, 8, "OUTLINE")
-    PlayerFrameManaBarTextRight:SetPoint("RIGHT", PlayerFrameManaBar, "RIGHT", -4, 0)
-    PlayerFrameManaBarTextRight:SetFont(STANDARD_TEXT_FONT, 8, "OUTLINE")
+    PlayerFrameManaBar:SetStatusBarTexture(T.BAR)
+    
+    local powerType = UnitPowerType("player")
+    
+    if powerType == 0 then -- Mana
+        PlayerFrameManaBar:SetStatusBarColor(unpack(B.RGB))
+    elseif powerType == 1 then -- Rage
+        PlayerFrameManaBar:SetStatusBarColor(unpack(R.RGB))
+    elseif powerType == 3 then -- Energy
+        PlayerFrameManaBar:SetStatusBarColor(unpack(Y.RGB))
+    end
 end
 
-local PlayerResourceEvents = CreateFrame("Frame")
-PlayerResourceEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
-PlayerResourceEvents:RegisterEvent("UNIT_MANA")
-PlayerResourceEvents:RegisterEvent("UNIT_HEALTH")
-PlayerResourceEvents:SetScript("OnEvent", function(self, event, arg1)
-    if event == "PLAYER_ENTERING_WORLD" or (arg1 == "player" and (event == "UNIT_MANA" or event == "UNIT_HEALTH")) then
-        UpdatePlayerResource()
+local PlayerResourceFrame = CreateFrame("Frame")
+PlayerResourceFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+PlayerResourceFrame:RegisterEvent("PLAYER_TARGET_CHANGED")
+PlayerResourceFrame:RegisterEvent("UNIT_HEALTH")
+PlayerResourceFrame:RegisterEvent("UNIT_HEALTH_FREQUENT")
+PlayerResourceFrame:RegisterEvent("UNIT_MAXHEALTH")
+PlayerResourceFrame:RegisterEvent("UNIT_POWER_UPDATE")
+PlayerResourceFrame:RegisterEvent("UNIT_MAXPOWER")
+PlayerResourceFrame:RegisterEvent("UNIT_DISPLAYPOWER")
+PlayerResourceFrame:SetScript("OnEvent", function(self, event, unit)
+    if event == "PLAYER_ENTERING_WORLD" or event == "PLAYER_TARGET_CHANGED" then
+        UpdatePlayerResources()
+    elseif unit == "player" then
+        UpdatePlayerResources()
     end
 end)
 
 
-
-
 -- UPDATE PLAYER PORTRAIT
 
-local function PlayerPortraitUpdate()
+local function UpdatePlayerPortrait()
     PlayerPortrait:ClearAllPoints()
     PlayerPortrait:SetPoint("CENTER", PlayerPortraitBackdrop, "CENTER", 0, 0)
-    PlayerPortrait:SetTexCoord(0.15, 0.85, 0.15, 0.85)
-    PlayerPortrait:SetSize(PlayerPortraitBackdrop:GetHeight()-6, PlayerPortraitBackdrop:GetHeight()-6)
+    PlayerPortrait:SetTexCoord(0.2, 0.8, 0.2, 0.8)
+    PlayerPortrait:SetSize(PlayerPortraitBackdrop:GetHeight() - 6, PlayerPortraitBackdrop:GetHeight() - 6)
     
     PlayerFrame:UnregisterEvent("UNIT_COMBAT")
 
@@ -121,263 +148,8 @@ local function PlayerPortraitUpdate()
     PlayerMasterIcon:ClearAllPoints()
     PlayerMasterIcon:SetPoint("BOTTOM", PlayerLeaderIcon, "TOP", 0, 0)
     PlayerMasterIcon:SetScale(0.75)
-
-    PlayerPVPTimerText:ClearAllPoints()
-    PlayerPVPTimerText:SetPoint("TOPRIGHT", PlayerPortraitBackdrop, "TOPLEFT", -4, 0)
-    PlayerPVPTimerText:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
-    PlayerPVPTimerText:SetTextColor(1, 1, 1, 1)
 end
 
-local PlayerPortraitEvents = CreateFrame("Frame")
-PlayerPortraitEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
-PlayerPortraitEvents:SetScript("OnEvent", PlayerPortraitUpdate)
-
-
-
-
--- UPDATE PLAYER GROUP ELEMENTS
-
-local function UpdateGroupElements()
-    PlayerFrameGroupIndicator:SetAlpha(0)
-    PlayerFrameGroupIndicator:Hide()
-
-    local MultiGroupFrame = _G["MultiGroupFrame"]
-    if MultiGroupFrame then
-        MultiGroupFrame:SetTexture(nil)
-        MultiGroupFrame:SetAlpha(0)
-        MultiGroupFrame:Hide()
-    end
-end
-
-if _G["MultiGroupFrame"] then
-    MultiGroupFrame:HookScript("OnShow", UpdateGroupElements)
-end
-
-PlayerFrameGroupIndicator:HookScript("OnShow", UpdateGroupElements)
-
-local PlayerGroupEvents = CreateFrame("Frame")
-PlayerGroupEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
-PlayerGroupEvents:SetScript("OnEvent", UpdateGroupElements)
-
-
-
-
--- UPDATE PLAYER LEVEL
-
-local function PlayerLevelUpdate()
-    PlayerLevelText:ClearAllPoints()
-    PlayerLevelText:SetPoint("TOP", PlayerPortraitBackdrop, "BOTTOM", 0, -4)
-    PlayerLevelText:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
-    PlayerLevelText:SetTextColor(1, 1, 1, 1)
-
-    if UnitLevel("player") < MAX_PLAYER_LEVEL then
-        PlayerLevelText:Show()
-    else
-        PlayerLevelText:Hide()
-    end
-end
-
-local PlayerLevelEvents = CreateFrame("Frame")
-PlayerLevelEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
-PlayerLevelEvents:RegisterEvent("PLAYER_LEVEL_UP")
-PlayerLevelEvents:RegisterEvent("PLAYER_XP_UPDATE")
-PlayerLevelEvents:SetScript("OnEvent", PlayerLevelUpdate)
-
-
-
-
--- GENERATE PET BACKDROP
-
-local PetFrameBackdrop = CreateFrame("Button", nil, PetFrame, "SecureUnitButtonTemplate, BackdropTemplate")
-PetFrameBackdrop:SetPoint("BOTTOMRIGHT", PlayerPortraitBackdrop, "BOTTOMLEFT", 0, 0)
-PetFrameBackdrop:SetSize(64, 24)
-PetFrameBackdrop:SetBackdrop({edgeFile = "Interface/Tooltips/UI-Tooltip-Border", tile = true, edgeSize = 12})
-PetFrameBackdrop:SetBackdropBorderColor(0.5, 0.5, 0.5, 1)
-PetFrameBackdrop:SetFrameStrata("HIGH")
-PetFrameBackdrop:SetAttribute("unit", "pet")
-PetFrameBackdrop:RegisterForClicks("AnyUp")
-PetFrameBackdrop:SetAttribute("type1", "target")
-PetFrameBackdrop:SetAttribute("type2", "togglemenu")
-
-
-
-
--- UPDATE PET FRAME
-
-local function UpdatePetFrame()
-	PetFrame:ClearAllPoints()
-	PetFrame:SetPoint("CENTER", PetFrameBackdrop, "CENTER", 0, 0)
-	PetFrame:SetSize(PetFrameBackdrop:GetWidth(), PetFrameBackdrop:GetHeight())
-    
-    PetFrameTexture:Hide()
-
-    PetPortrait:Hide()
-	PetAttackModeTexture:SetTexture(nil)
-
-    PetName:Hide()
-	
-	PetFrameHealthBar:ClearAllPoints()
-	PetFrameHealthBar:SetPoint("BOTTOM", PetFrameManaBar, "TOP", 0, 0)
-	PetFrameHealthBar:SetPoint("TOP", PetFrameBackdrop, "TOP", 0, -2)
-	PetFrameHealthBar:SetWidth(PetFrameBackdrop:GetWidth()-4)
-	PetFrameHealthBar:SetStatusBarTexture("Interface/RaidFrame/Raid-Bar-HP-Fill.blp")
-	
-	PetFrameManaBar:ClearAllPoints()
-	PetFrameManaBar:SetPoint("BOTTOM", PetFrameBackdrop, "BOTTOM", 0, 2)
-	PetFrameManaBar:SetHeight(8)
-	PetFrameManaBar:SetWidth(PetFrameBackdrop:GetWidth()-4)
-	PetFrameManaBar:SetStatusBarTexture("Interface/RaidFrame/Raid-Bar-HP-Fill.blp")
-	
-	PetFrameHealthBarText:SetAlpha(0)
-	PetFrameHealthBarTextLeft:SetAlpha(0)
-	PetFrameHealthBarTextRight:SetAlpha(0)
-	PetFrameManaBarText:SetAlpha(0)
-	PetFrameManaBarTextLeft:SetAlpha(0)
-	PetFrameManaBarTextRight:SetAlpha(0)
-	
-	PetFrame:UnregisterEvent("UNIT_COMBAT")
-	
-	PetFrameHappiness:ClearAllPoints()
-	PetFrameHappiness:SetPoint("RIGHT", PetFrameBackdrop, "LEFT", 0, 0)
-	
-	for i = 1, MAX_TARGET_BUFFS do
-		local PetBuff = _G["PetFrameBuff" .. i]
-		if PetBuff then
-			PetBuff:SetAlpha(0)
-		end
-	end
-	
-	for i = 1, MAX_TARGET_DEBUFFS do
-		local PetDebuff = _G["PetFrameDebuff" .. i]
-		if PetDebuff then
-			PetDebuff:SetAlpha(0)
-		end
-	end
-end
-
-local PetFrameEvents = CreateFrame("Frame")
-PetFrameEvents:RegisterEvent("UNIT_PET")
-PetFrameEvents:SetScript("OnEvent", UpdatePetFrame)
-
-
-
-
--- ENABLE DRUID MANA BAR
-
-local _, ClassIdentifier = UnitClass("player")
-
-if ClassIdentifier == "DRUID" then
-
-    local DruidManaBarBackdrop = CreateFrame("Frame", nil, PlayerFrame, "BackdropTemplate")
-    DruidManaBarBackdrop:SetPoint("TOP", PlayerFrameBackdrop, "BOTTOM", 0, 0)
-    DruidManaBarBackdrop:SetSize(PlayerFrameBackdrop:GetWidth() - 2, 20)
-    DruidManaBarBackdrop:SetBackdrop({edgeFile = "Interface/Tooltips/UI-Tooltip-Border", edgeSize = 12, })
-    DruidManaBarBackdrop:SetBackdropBorderColor(0.5, 0.5, 0.5, 1)
-
-    local DruidManaBar = CreateFrame("StatusBar", nil, PlayerFrame, "BackdropTemplate")
-    DruidManaBar:SetSize(DruidManaBarBackdrop:GetWidth()-4, DruidManaBarBackdrop:GetHeight()-4)
-    DruidManaBar:SetPoint("CENTER", DruidManaBarBackdrop, "CENTER", 0, 0)
-    DruidManaBar:SetStatusBarTexture("Interface/RaidFrame/Raid-Bar-HP-Fill.blp")
-    DruidManaBar:SetStatusBarColor(0, 0.25, 1)
-    DruidManaBar:SetMinMaxValues(0, 1)
-
-    DruidManaBarBackdrop:SetFrameLevel(DruidManaBar:GetFrameLevel() + 1)
-
-    local DruidManaText = DruidManaBar:CreateFontString(nil, "OVERLAY", "GameFontNormalSmall")
-    DruidManaText:SetPoint("CENTER", DruidManaBar, "CENTER", 0, 0)
-    DruidManaText:SetTextColor(1, 1, 1)
-    DruidManaText:SetFont(GameFontNormal:GetFont(), 10)
-
-    local function DruidManaBarUpdate()
-        local PlayerPowerType, PowerToken = UnitPowerType("player")
-        local PlayerForm = GetShapeshiftFormID()
-    
-        if PlayerForm and PowerToken ~= "MANA" then
-            local PlayerMana = UnitPower("player", Enum.PowerType.Mana)
-            local PlayerMaxMana = UnitPowerMax("player", Enum.PowerType.Mana)
-            DruidManaBar:SetValue(PlayerMana / PlayerMaxMana)
-            DruidManaText:SetText(PlayerMana .. " / " .. PlayerMaxMana)
-            DruidManaBar:Show()
-            DruidManaBarBackdrop:Show()
-        else
-            DruidManaBar:Hide()
-            DruidManaBarBackdrop:Hide()
-        end
-    end
-
-    local DruidManaBarEvents = CreateFrame("Frame")
-    DruidManaBarEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
-    DruidManaBarEvents:RegisterEvent("UNIT_POWER_UPDATE")
-    DruidManaBarEvents:RegisterEvent("UNIT_DISPLAYPOWER")
-    DruidManaBarEvents:SetScript("OnEvent", DruidManaBarUpdate)
-end
-
-
-
-
--- UPDATE ROGUE COMBO POINTS
-
-local _, ClassIdentifier = UnitClass("player")
-if ClassIdentifier ~= "ROGUE" and ClassIdentifier ~= "DRUID" then
-    return
-end
-
-local PointSize = 24
-local PointMargin = 4
-local PointsTotalWidth = 5 * PointSize + 4 * PointMargin
-
-local ComboPointsFrame = CreateFrame("Frame", "ComboPointsFrame", UIParent)
-ComboPointsFrame:SetSize(PointsTotalWidth, PointSize)
-ComboPointsFrame:SetPoint("BOTTOM", CastingBarFrame, "TOP", 0, 4)
-
-local ComboPoints = {}
-
-local function CreateComboPoint()
-    local cp = CreateFrame("Frame", nil, ComboPointsFrame, "BackdropTemplate")
-    cp:SetSize(PointSize, PointSize)
-    cp:SetBackdrop({
-        bgFile = "Interface/ChatFrame/ChatFrameBackground",
-        edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
-        tile = false,
-        tileSize = 16,
-        edgeSize = 12,
-        insets = {left = 2, right = 2, top = 2, bottom = 2}
-    })
-    cp:SetBackdropBorderColor(0.5, 0.5, 0.5, 1)
-    cp:SetBackdropColor(0, 0, 0, 0.5)
-    return cp
-end
-
-local function ComboPointTextures(cp, active)
-    if active then
-        cp:SetBackdropColor(0.75, 0, 0, 1)
-    else
-        cp:SetBackdropColor(0, 0, 0, 0.5)
-    end
-end
-
-for i = 1, 5 do
-    ComboPoints[i] = CreateComboPoint()
-    ComboPointTextures(ComboPoints[i], false)
-    ComboPoints[i]:SetPoint("LEFT", ComboPointsFrame, "LEFT", PointSize * (i - 1) + PointMargin * (i - 1), 0)
-end
-
-local function ComboPointsUpdate()
-    ComboFrame:UnregisterAllEvents()
-    ComboFrame:Hide()
-    local count = GetComboPoints("player", "target") or 0
-    if count > 0 then
-        ComboPointsFrame:Show()
-        for i = 1, 5 do
-            ComboPointTextures(ComboPoints[i], i <= count)
-        end
-    else
-        ComboPointsFrame:Hide()
-    end
-end
-
-local ComboPointsEvents = CreateFrame("Frame")
-ComboPointsEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
-ComboPointsEvents:RegisterEvent("PLAYER_TARGET_CHANGED")
-ComboPointsEvents:RegisterUnitEvent("UNIT_POWER_UPDATE", "player")
-ComboPointsEvents:SetScript("OnEvent", ComboPointsUpdate)
+local PlayerPortraitFrame = CreateFrame("Frame")
+PlayerPortraitFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+PlayerPortraitFrame:SetScript("OnEvent", UpdatePlayerPortrait)
