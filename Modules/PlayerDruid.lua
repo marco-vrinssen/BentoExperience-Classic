77f63dec4c608f7682248d53ceb397dbd3945ae3
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-02-27 11:26:21 +0100

    Add PlayerDruid module: implement Druid mana bar display and updates

---
+++ Modules/PlayerDruid.lua
@@ -0,0 +1,49 @@
+-- ENABLE DRUID MANA BAR
+
+local _, ClassIdentifier = UnitClass("player")
+
+if ClassIdentifier == "DRUID" then
+
+    local DruidManaBarBackdrop = CreateFrame("Frame", nil, PlayerFrame, "BackdropTemplate")
+    DruidManaBarBackdrop:SetPoint("TOP", PlayerFrameBackdrop, "BOTTOM", 0, 0)
+    DruidManaBarBackdrop:SetSize(PlayerFrameBackdrop:GetWidth() - 2, 20)
+    DruidManaBarBackdrop:SetBackdrop({edgeFile = T.EDGE, edgeSize = T.EDGE_SIZE, })
+    DruidManaBarBackdrop:SetBackdropBorderColor(unpack(N.RGB))
+
+    local DruidManaBar = CreateFrame("StatusBar", nil, PlayerFrame, "BackdropTemplate")
+    DruidManaBar:SetSize(DruidManaBarBackdrop:GetWidth()-4, DruidManaBarBackdrop:GetHeight()-4)
+    DruidManaBar:SetPoint("CENTER", DruidManaBarBackdrop, "CENTER", 0, 0)
+    DruidManaBar:SetStatusBarTexture(T.BAR)
+    DruidManaBar:SetStatusBarColor(unpack(B.RGB))
+    DruidManaBar:SetMinMaxValues(0, 1)
+
+    DruidManaBarBackdrop:SetFrameLevel(DruidManaBar:GetFrameLevel() + 1)
+
+    local DruidManaText = DruidManaBar:CreateFontString(nil, "OVERLAY", "GameFontNormalSmall")
+    DruidManaText:SetPoint("CENTER", DruidManaBar, "CENTER", 0, 0)
+    DruidManaText:SetTextColor(1, 1, 1)
+    DruidManaText:SetFont(GameFontNormal:GetFont(), 10)
+
+    local function DruidManaBarUpdate()
+        local PlayerPowerType, PowerToken = UnitPowerType("player")
+        local PlayerForm = GetShapeshiftFormID()
+    
+        if PlayerForm and PowerToken ~= "MANA" then
+            local PlayerMana = UnitPower("player", Enum.PowerType.Mana)
+            local PlayerMaxMana = UnitPowerMax("player", Enum.PowerType.Mana)
+            DruidManaBar:SetValue(PlayerMana / PlayerMaxMana)
+            DruidManaText:SetText(PlayerMana .. " / " .. PlayerMaxMana)
+            DruidManaBar:Show()
+            DruidManaBarBackdrop:Show()
+        else
+            DruidManaBar:Hide()
+            DruidManaBarBackdrop:Hide()
+        end
+    end
+
+    local DruidManaBarFrame = CreateFrame("Frame")
+    DruidManaBarFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+    DruidManaBarFrame:RegisterEvent("UNIT_POWER_UPDATE")
+    DruidManaBarFrame:RegisterEvent("UNIT_DISPLAYPOWER")
+    DruidManaBarFrame:SetScript("OnEvent", DruidManaBarUpdate)
+end
