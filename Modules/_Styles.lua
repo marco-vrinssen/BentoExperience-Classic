c77743ecfdad470e653bbf387dd87e3e6e0aaa36
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-02-27 11:24:22 +0100

    Add Styles module: define color constants, textures, and font settings for improved UI customization

---
+++ Modules/_Styles.lua
@@ -0,0 +1,91 @@
+-- COLORS
+
+W = {                               -- #FFFFFF
+    RGB = {1, 1, 1},               
+    LUA = "|cFFFFFFFF"             
+}
+
+D = {                               -- #000000
+    RGB = {0, 0, 0},               
+    LUA = "|cFF000000"             
+}
+
+N = {                               -- #808080
+    RGB = {0.5, 0.5, 0.5},         
+    LUA = "|cFF808080"             
+}
+
+Y = {                               -- #FBD134
+    RGB = {0.984, 0.820, 0.204},   
+    LUA = "|cFFFBD134"             
+}
+
+YL = {                              -- #FDFE9E
+    RGB = {0.992, 0.996, 0.620},   
+    LUA = "|cFFFDFE9E"             
+}
+
+O = {                               -- #FB8128
+    RGB = {0.984, 0.506, 0.157},   
+    LUA = "|cFFFB8128"             
+}
+
+R = {                               -- #EA2029
+    RGB = {0.918, 0.125, 0.161},   
+    LUA = "|cFFEA2029"             
+}
+
+P = {                               -- #FB18B2
+    RGB = {0.984, 0.094, 0.698},   
+    LUA = "|cFFFB18B2"             
+}
+
+PL = {                              -- #FFCCFE
+    RGB = {1.000, 0.800, 0.996},   
+    LUA = "|cFFFFCCFE"             
+}
+
+V = {                               -- #A43BEA
+    RGB = {0.643, 0.231, 0.918},   
+    LUA = "|cFFA43BEA"             
+}
+
+BL = {                              -- #2871D9
+    RGB = {0.157, 0.443, 0.851},   
+    LUA = "|cFF2871D9"             
+}
+
+B = {                               -- #220FC9
+    RGB = {0.133, 0.059, 0.788},   
+    LUA = "|cFF220FC9"             
+}
+
+G = {                               -- #00C800
+    RGB = {0.0, 0.784, 0.0},       
+    LUA = "|cFF00C800"             
+}
+
+GD = {                              -- #119826
+    RGB = {0.067, 0.596, 0.149},    
+    LUA = "|cFF119826"             
+}
+
+
+-- TEXTURES
+
+T = {
+    BAR = "Interface/AddOns/BentoExperience-Classic/Assets/Statusbar.blp",
+    BG = "Interface/Tooltips/UI-Tooltip-Background.blp",
+    EDGE = "Interface/Tooltips/UI-Tooltip-Border.blp",
+    EDGE_SIZE = 12,
+}
+
+
+-- FONTS
+
+F = {
+    TYPE = STANDARD_TEXT_FONT,
+    SIZE = 12,
+    SIZE_SMALL = 10,
+    SIZE_LARGE = 14,
+}
