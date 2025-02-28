97608d9ad37ed57d6a85b58147532fd0ba9916b2
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-02-28 08:17:20 +0100

    Update PlayerPet styling: adjust backdrop settings, border colors, and add pet happiness display for improved clarity

--- Modules/PlayerPet.lua
+++ Modules/PlayerPet.lua
@@ -3,9 +3,9 @@
 local PetFrameBackdrop = CreateFrame("Button", nil, PetFrame, "SecureUnitButtonTemplate, BackdropTemplate")
 PetFrameBackdrop:SetPoint("BOTTOMRIGHT", PlayerPortraitBackdrop, "BOTTOMLEFT", 0, 0)
 PetFrameBackdrop:SetSize(64, 24)
-PetFrameBackdrop:SetBackdrop({edgeFile = T.EDGE, edgeSize = T.EDGE_SIZE})
-PetFrameBackdrop:SetBackdropBorderColor(unpack(N.RGB))
-PetFrameBackdrop:SetFrameStrata("HIGH")
+PetFrameBackdrop:SetBackdrop({edgeFile = EDGE, edgeSize = MEDIUM})
+PetFrameBackdrop:SetBackdropBorderColor(unpack(GREY))
+PetFrameBackdrop:SetFrameLevel(PetFrame:GetFrameLevel() + 2)
 PetFrameBackdrop:SetAttribute("unit", "pet")
 PetFrameBackdrop:RegisterForClicks("AnyUp")
 PetFrameBackdrop:SetAttribute("type1", "target")
@@ -18,18 +18,16 @@ local function UpdatePetFrame()
 	PetFrame:ClearAllPoints()
 	PetFrame:SetPoint("CENTER", PetFrameBackdrop, "CENTER", 0, 0)
 	PetFrame:SetSize(PetFrameBackdrop:GetWidth(), PetFrameBackdrop:GetHeight())
-    
-    PetFrameTexture:Hide()
-
-    PetPortrait:Hide()
+    PetFrame:UnregisterEvent("UNIT_COMBAT")
 	PetAttackModeTexture:SetTexture(nil)
+    PetFrameTexture:Hide()
+   
+    PetPortrait:Hide()
 
-    PetName:Hide()
-	
-	PetFrame:UnregisterEvent("UNIT_COMBAT")
-	
-	PetFrameHappiness:ClearAllPoints()
-	PetFrameHappiness:SetPoint("RIGHT", PetFrameBackdrop, "LEFT", 0, 0)
+    PetName:ClearAllPoints()
+    PetName:SetPoint("BOTTOMRIGHT", PetFrameBackdrop, "TOPRIGHT", 0, 2)
+    PetName:SetFont(FONT, SMALL, "OUTLINE")
+    PetName:SetTextColor(unpack(WHITE))
 
 	for i = 1, MAX_TARGET_BUFFS do
 		local PetBuff = _G["PetFrameBuff" .. i]
@@ -55,18 +53,17 @@ PetFrameFrame:SetScript("OnEvent", UpdatePetFrame)
 -- UPDATE PET RESOURCES
 
 local function UpdatePetResources()
-    
     PetFrameHealthBar:ClearAllPoints()
     PetFrameHealthBar:SetPoint("TOP", PetFrameBackdrop, "TOP", 0, -2)
     PetFrameHealthBar:SetPoint("BOTTOM", PetFrameManaBar, "TOP", 0, 0)
     PetFrameHealthBar:SetWidth(PetFrameBackdrop:GetWidth()-6)
-    PetFrameHealthBar:SetStatusBarTexture(T.BAR)
+    PetFrameHealthBar:SetStatusBarTexture(BAR)
     
     PetFrameManaBar:ClearAllPoints()
     PetFrameManaBar:SetPoint("BOTTOM", PetFrameBackdrop, "BOTTOM", 0, 2)
     PetFrameManaBar:SetWidth(PetFrameBackdrop:GetWidth()-6)
     PetFrameManaBar:SetHeight(8)
-    PetFrameManaBar:SetStatusBarTexture(T.BAR)
+    PetFrameManaBar:SetStatusBarTexture(BAR)
     
     PetFrameHealthBarText:SetAlpha(0)
     PetFrameHealthBarTextLeft:SetAlpha(0)
@@ -85,5 +82,44 @@ PetResourceFrame:SetScript("OnEvent", function(self, event, unit)
     if event == "PLAYER_ENTERING_WORLD" or event == "UNIT_PET" or 
        (event == "UNIT_POWER_UPDATE" and unit == "pet") then
         UpdatePetResources()
+    end
+end)
+
+
+-- CREATE PET HAPPINESS BACKDROP
+
+local PetHappinessBackdrop = CreateFrame("Frame", nil, PetFrame, "BackdropTemplate")
+PetHappinessBackdrop:SetSize(24, 24)
+PetHappinessBackdrop:SetPoint("RIGHT", PetFrameBackdrop, "LEFT", 0, 0)
+PetHappinessBackdrop:SetBackdrop({
+    bgFile = "Interface\\Icons\\ability_hunter_beasttraining",
+    edgeFile = EDGE, edgeSize = MEDIUM,
+    insets = {left = 2, right = 2, top = 2, bottom = 2}
+})
+PetHappinessBackdrop:SetBackdropBorderColor(unpack(GREY))
+PetHappinessBackdrop:SetFrameLevel(PetFrame:GetFrameLevel() + 1)
+
+
+-- UPDATE PET HAPPINESS
+
+local function UpdatePetHappiness()
+    PetFrameHappiness:Hide()
+    PetFrameHappinessTexture:Hide()
+
+    local happiness = GetPetHappiness()
+    if happiness and happiness < 3 then
+        PetHappinessBackdrop:Show()
+    else
+        PetHappinessBackdrop:Hide()
+    end
+end
+
+local PetHappinessFrame = CreateFrame("Frame")
+PetHappinessFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+PetHappinessFrame:RegisterEvent("UNIT_HAPPINESS")
+PetHappinessFrame:RegisterEvent("UNIT_PET")
+PetHappinessFrame:SetScript("OnEvent", function(self, event, unit)
+    if unit == "pet" or event == "PLAYER_ENTERING_WORLD" then
+        UpdatePetHappiness()
     end
 end)
