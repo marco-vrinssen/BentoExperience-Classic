19734ac6b9f2b2725b1d7b7db931fca5908cfde6
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-02-28 08:15:10 +0100

    Update casting bar styling: adjust backdrop settings, border color, and frame level for improved appearance

--- Modules/CastBar.lua
+++ Modules/CastBar.lua
@@ -3,9 +3,9 @@
 local CastingBarBackdrop = CreateFrame("Frame", nil, CastingBarFrame, "BackdropTemplate")
 CastingBarBackdrop:SetPoint("TOPLEFT", CastingBarFrame, "TOPLEFT", -3, 3)
 CastingBarBackdrop:SetPoint("BOTTOMRIGHT", CastingBarFrame, "BOTTOMRIGHT", 3, -3)
-CastingBarBackdrop:SetBackdrop({ edgeFile = T.EDGE, edgeSize = T.EDGE_SIZE})
-CastingBarBackdrop:SetBackdropBorderColor(unpack(N.RGB))
-CastingBarBackdrop:SetFrameStrata("HIGH")
+CastingBarBackdrop:SetBackdrop({ edgeFile = EDGE, edgeSize = MEDIUM})
+CastingBarBackdrop:SetBackdropBorderColor(unpack(GREY))
+CastingBarBackdrop:SetFrameLevel(CastingBarFrame:GetFrameLevel() + 1)
 
 
 -- UPDATE CASTBAR
@@ -15,19 +15,19 @@ local function UpdateCastBar()
     CastingBarFrame:SetSize(160, 24)
     CastingBarFrame:SetMovable(true)
     CastingBarFrame:SetUserPlaced(true)
-    CastingBarFrame:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, 240)
-    CastingBarFrame:SetStatusBarTexture(T.BAR)
+    CastingBarFrame:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, 242)
+    CastingBarFrame:SetStatusBarTexture(BAR)
     CastingBarFrame.Border:Hide()
     CastingBarFrame.Spark:SetTexture(nil)
     CastingBarFrame.Flash:SetTexture(nil)
     CastingBarFrame.Text:ClearAllPoints()
     CastingBarFrame.Text:SetPoint("CENTER", CastingBarFrame, "CENTER", 0, 0)
-    CastingBarFrame.Text:SetFont(F.TYPE, F.SIZE, "OUTLINE")
+    CastingBarFrame.Text:SetFont(FONT, MEDIUM, "OUTLINE")
 end
 
 local function RecolorCastBar(event)
     if event == "UNIT_SPELLCAST_START" then
-        CastingBarFrame:SetStatusBarColor(unpack(Y.RGB))
+        CastingBarFrame:SetStatusBarColor(unpack(YELLOW))
     end
 end  
 
