cebbb343a8f5263dab0e5b7f162631031cfdf615
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-03-03 22:15:30 +0100

    Refactor Menu module functions for consistent naming and improved readability

--- Modules/Menu.lua
+++ Modules/Menu.lua
@@ -1,19 +1,19 @@
--- UPDATE MICRO MENU POSITIONING
+-- CUSTOMIZE MICRO MENU BUTTONS
 
-local function UpdateMicroMenuButtons()
-  local microButtons = {}
+local function updateMenuButtons()
+  local menuButtons = {}
 
   for name in pairs(_G) do
     if type(name) == "string" and string.match(name, "^%a+MicroButton$") then
       local button = _G[name]
       if type(button) == "table" and button.IsVisible and button:IsVisible() then
-        table.insert(microButtons, button)
+        table.insert(menuButtons, button)
       end
     end
   end
   
   local buttonWidth = CharacterMicroButton:GetWidth()
-  local totalMenuWidth = buttonWidth * (#microButtons - 1)
+  local totalMenuWidth = buttonWidth * (#menuButtons - 1)
   
   CharacterMicroButton:ClearAllPoints()
   CharacterMicroButton:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", -totalMenuWidth, 16)
@@ -22,14 +22,14 @@ end
 
 -- DELAYED MICROMENU UPDATE
 
-local function DelayedMicroMenuUpdate()
-  C_Timer.After(0.1, UpdateMicroMenuButtons)
+local function delayUpdate()
+  C_Timer.After(0.1, updateMenuButtons)
 end
 
 local microMenuEvents = CreateFrame("Frame")
 microMenuEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
 microMenuEvents:RegisterEvent("PLAYER_LOGIN")
-microMenuEvents:SetScript("OnEvent", DelayedMicroMenuUpdate)
+microMenuEvents:SetScript("OnEvent", delayUpdate)
 
 
 -- RELOAD UI ON GAME MICRO MENU RIGHT CLICK
@@ -43,22 +43,24 @@ end)
 
 -- UPDATE LFG BUTTON
 
-local function MinimapLFGUpdate()
+local function updateMinimapAndLFG()
   if LFGMinimapFrameBorder then
       LFGMinimapFrameBorder:Hide()
   end
+
   if LFGMinimapFrame then
       LFGMinimapFrame:SetParent(Minimap)
       LFGMinimapFrame:ClearAllPoints()
-      LFGMinimapFrame:SetSize(44, 44)
-      LFGMinimapFrame:SetPoint("RIGHT", CharacterMicroButton, "LEFT", 4, -8)
+      LFGMinimapFrame:SetSize(36, 36)
+      LFGMinimapFrame:SetPoint("RIGHT", CharacterMicroButton, "LEFT", -8, -8)
   end
+
   if LFGMinimapFrameIcon then
       LFGMinimapFrameIcon:SetSize(40, 40)
       LFGMinimapFrameIcon:SetPoint("CENTER", LFGMinimapFrame, "CENTER", 0, 0)
   end
 end
 
-local MinimapLFGFrame = CreateFrame("Frame")
-MinimapLFGFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
-MinimapLFGFrame:SetScript("OnEvent", MinimapLFGUpdate)
+local minimapLFGEvents = CreateFrame("Frame")
+minimapLFGEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
+minimapLFGEvents:SetScript("OnEvent", updateMinimapAndLFG)
