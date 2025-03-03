dd3f1c46640eddf6b0b6590eb10eaee33c6b3691
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-03-03 22:15:58 +0100

    Refactor Tooltips module for consistent naming and improved readability

--- Modules/Tooltips.lua
+++ Modules/Tooltips.lua
@@ -1,14 +1,14 @@
--- UPDATE TOOLTIP
+-- UPDATE TOOLTIP POSITION
 
-local function UpdateTooltip(self)
+local function updateTooltipPos(self)
     if self:GetAnchorType() ~= "ANCHOR_CURSOR" then
         GameTooltip:ClearAllPoints()
         GameTooltip:SetPoint("TOPLEFT", TargetFramePortrait, "BOTTOMRIGHT", 4, -4)
     end
 end
 
-GameTooltipStatusBar:SetScript("OnShow", function(self)
-    self:Hide()
+GameTooltipStatusBar:SetScript("OnShow", function()
+    GameTooltipStatusBar:Hide()
 end)
 
-hooksecurefunc("GameTooltip_SetDefaultAnchor", UpdateTooltip)
+hooksecurefunc("GameTooltip_SetDefaultAnchor", updateTooltipPos)
