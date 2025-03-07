fd8e6ca10504ca66d51dd714407ba0791ad2546d
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-03-07 14:49:48 +0100

    Refactor dialog event handling in Dialogs.lua for improved structure and readability

--- Modules/Dialogs.lua
+++ Modules/Dialogs.lua
@@ -1,33 +1,49 @@
 -- AUTO CONFIRM DIALOGS
 
-local function confirmDialog(event, arg1, arg2, ...)
-    if event == "CONFIRM_LOOT_ROLL" or event == "CONFIRM_DISENCHANT_ROLL" then
+local StaticPopup_Hide = StaticPopup_Hide
+
+
+-- DIALOG HANDLERS
+
+local eventHandlers = {
+    CONFIRM_LOOT_ROLL = function(arg1, arg2)
         ConfirmLootRoll(arg1, arg2)
         StaticPopup_Hide("CONFIRM_LOOT_ROLL")
-        return
-    end
-
-    if event == "LOOT_BIND_CONFIRM" then
+    end,
+    
+    LOOT_BIND_CONFIRM = function(arg1, arg2, ...)
         ConfirmLootSlot(arg1, arg2)
-        StaticPopup_Hide("LOOT_BIND",...)
-        return
-    end
-
-    if event == "MERCHANT_CONFIRM_TRADE_TIMER_REMOVAL" then
-        SellCursorItem()
-        return
-    end
-
-    if event == "MAIL_LOCK_SEND_ITEMS" then
+        StaticPopup_Hide("LOOT_BIND", ...)
+    end,
+    
+    MERCHANT_CONFIRM_TRADE_TIMER_REMOVAL = SellCursorItem,
+    
+    MAIL_LOCK_SEND_ITEMS = function(arg1)
         RespondMailLockSendItem(arg1, true)
-        return
-    end
-end
+    end,
+    
+    EQUIP_BIND_CONFIRM = function(arg1)
+        EquipPendingItem(arg1)
+        StaticPopup_Hide("EQUIP_BIND")
+    end,
+    
+    EQUIP_BIND_TRADEABLE_CONFIRM = function(arg1)
+        StaticPopupDialogs["EQUIP_BIND_TRADEABLE"]:OnAccept(arg1)
+        StaticPopup_Hide("EQUIP_BIND_TRADEABLE")
+    end,
+    
+    DELETE_ITEM_CONFIRM = DeleteCursorItem
+}
 
-local confirmationEvents = CreateFrame("Frame")
-confirmationEvents:RegisterEvent("CONFIRM_LOOT_ROLL")
-confirmationEvents:RegisterEvent("CONFIRM_DISENCHANT_ROLL")
-confirmationEvents:RegisterEvent("LOOT_BIND_CONFIRM")
-confirmationEvents:RegisterEvent("MERCHANT_CONFIRM_TRADE_TIMER_REMOVAL")
-confirmationEvents:RegisterEvent("MAIL_LOCK_SEND_ITEMS")
-confirmationEvents:SetScript("OnEvent", confirmDialog)
+
+-- SETUP EVENT HANDLING
+
+local frame = CreateFrame("Frame")
+frame:SetScript("OnEvent", function(_, event, arg1, arg2, ...)
+    local handler = eventHandlers[event]
+    if handler then handler(arg1, arg2, ...) end
+end)
+
+for event in pairs(eventHandlers) do
+    frame:RegisterEvent(event)
+end
